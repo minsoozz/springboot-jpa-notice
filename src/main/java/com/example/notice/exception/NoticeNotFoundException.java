8a4e80117ba488e1c709eff775bc70909bf09fcf
minsoozz <minsoo1604@naver.com>
2021-12-24 19:24:24 +0900

    공지사항 커스텀 익셉션 추가
    - 게시글 조회시 반환이 없을 경우 커스텀 익셉션 반환

---
+++ src/main/java/com/example/notice/exception/NoticeNotFoundException.java
@@ -0,0 +1,9 @@
+package com.example.notice.exception;
+
+public class NoticeNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 2095453925401290605L;
+
+    public NoticeNotFoundException() {
+    }
+}
