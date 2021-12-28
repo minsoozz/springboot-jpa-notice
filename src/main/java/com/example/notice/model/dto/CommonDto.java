518f82feecfb4907b5951d0dce08f5be645aa2e2
minsoozz <minsoo1604@naver.com>
2021-12-28 21:52:58 +0900

    LocalDateTime 직렬화/역직렬화 설정
    - Redis 사용을 위해 어노테이션 추가

--- src/main/java/com/example/notice/model/dto/CommonDto.java
+++ src/main/java/com/example/notice/model/dto/CommonDto.java
@@ -1,5 +1,9 @@
 package com.example.notice.model.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +15,8 @@ import org.springframework.format.annotation.DateTimeFormat;
 @NoArgsConstructor
 public abstract class CommonDto {
 
+  @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+  @JsonSerialize(using = LocalDateTimeSerializer.class)
   @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
   private LocalDateTime createdDate;
 
